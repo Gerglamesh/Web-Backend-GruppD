09906ad3ee4bbc7e8ec1ec41de83d83466e84a97
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-04-30 10:09:30 +0200

    @Gerglamesh, add environment variabels setup up, configure .ignore to ignore environment specific setup, add ConnectionSetup Class to store config of environment specific connection string

---
+++ TravelAPI/ConnectionSetup.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace EngineClasses
+{
+    public static class ConnectionSetup
+    {
+        public static string GetConnectionString()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("SettingsEnvironment");
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("EnvVars.json").AddJsonFile($"EnvVars.{environmentName}.json", optional: true);
+            var config = builder.Build();
+
+            return config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
--- TravelAPI/Program.cs
+++ TravelAPI/Program.cs
@@ -14,6 +14,7 @@ namespace TravelAPI
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
