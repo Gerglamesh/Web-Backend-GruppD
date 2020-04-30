53e9eed14b131ee0c264135aec6a8302145d8599
irvin <irvin_b23@hotmail.com>
2020-04-30 11:45:34 +0200

    Create DbContext

---
+++ TravelAPI/FakeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EngineClasses
+
+
+namespace TravelAPI
+{
+    public class FakeContext : DbContext
+    {
+        public DbSet<Session> Session { get; set; }
+        public DbSet<Player> Player { get; set; }
+        public DbSet<GamePiece> GamePiece { get; set; }
+        public DbSet<GameLog> GameLog { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionSetup.GetConnectionString());
+        }
+     }
+}
