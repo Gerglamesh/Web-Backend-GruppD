fc7116167e1ba64c91317d0d977fae575a853fef
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-04-30 13:59:39 +0200

    @MirkoPralica resolved merge conflict

--- TravelAPI/Controller/ControllerPrototype.cs
+++ TravelAPI/Controller/ControllerPrototype.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TravelAPI.Controller
 {
-    public class Class
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class PrototypeController : ControllerBase
     {
+        [HttpGet]
+        public string Get()
+        {
+            // Anrop till Fake Repo
+            return "Hi there";
+        }
     }
 }
--- TravelAPI/Startup.cs
+++ TravelAPI/Startup.cs
@@ -16,6 +16,7 @@ namespace TravelAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,8 @@ namespace TravelAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseMvc();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
