2e70b3fd454986201e9fbc7f2ef9567aa531ba92
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-05 13:19:35 +0200

    @Gerglamesh, Add CountryId prop to CountryModel

---
+++ TravelAPI/Models/CountryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.Models
+{
+    public class CountryModel
+    {
+        [Key]
+        public int CountryId { get; private set; }
+
+    }
+}
