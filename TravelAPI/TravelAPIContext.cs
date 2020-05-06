2575f0294b9eccd973e8b830aca38d5b6f0f4874
irvin <irvin_b23@hotmail.com>
2020-05-06 15:49:17 +0200

    @irvper77 add test data to travelapicontext

--- TravelAPI/TravelAPIContext.cs
+++ TravelAPI/TravelAPIContext.cs
@@ -1,21 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using TravelAPI.Controller;
-=======
->>>>>>> c27c810beabc2db4a3acc76c5cd81d0a00ff3e76
 using TravelAPI.Models;
 
 namespace TravelAPI
 {
     public class TravelAPIContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+        public TravelAPIContext (IConfiguration config, DbContextOptions options) : base(options)
+        {
+            _configuration = config;
+        }
+
         public DbSet<CountryModel> CountryModel { get; set; }
         public DbSet<CountryInfoModel> CountryInfoModel { get; set; }
         public DbSet<AttractionModel> AttractionModel { get; set; }
         public DbSet<CityModel> CityModel { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("TravelAPIContext"));
+        }
+
+
     }
 }
