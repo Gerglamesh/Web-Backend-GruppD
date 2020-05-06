facf11891020ff4fb6efad255770392c141d7eff
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-05-06 14:43:37 +0200

    @MirkoPralica Add Repo in Services

---
+++ TravelAPI/Services/CountryInfoRepo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAPI.Models;
+
+namespace TravelAPI.Services
+{
+    public class CountryInfoRepo : ICountryInfoRepo
+    {
+        private readonly TravelAPIContext dbcontext;
+        public CountryInfoRepo(TravelAPIContext Context)
+        {
+            dbcontext = Context;
+        }
+
+        public async Task<CountryInfoModel> GetCountryInfoModel()
+        {
+            var query = dbcontext.CountryInfoModel;
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
