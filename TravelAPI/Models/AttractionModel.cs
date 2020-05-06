8f32680601409f3d782916f627f1309f21f6debf
irvin <irvin_b23@hotmail.com>
2020-05-06 13:43:33 +0200

    @irvper77 TRAPI-131 TRAPI-128 TRAPI-133

---
+++ TravelAPI/Models/AttractionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelAPI.Models
+{
+    public class AttractionModel
+    {
+        [Key]
+        
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Childfriendly { get; set; }
+        public bool Rating { get; set; }
+        
+        //Relationships
+        public CityModel City { get; private set; }
+    }
+}
