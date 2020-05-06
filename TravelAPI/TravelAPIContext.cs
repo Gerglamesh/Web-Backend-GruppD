6aab5766212493d7e48b8ba559b2f41918b0743d
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-06 15:42:12 +0200

    @Gerglamesh, resolved merge conflict in TravelAPIContext.cs

--- TravelAPI/TravelAPIContext.cs
+++ TravelAPI/TravelAPIContext.cs
@@ -3,10 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using TravelAPI.Controller;
-=======
->>>>>>> c27c810beabc2db4a3acc76c5cd81d0a00ff3e76
 using TravelAPI.Models;
 
 namespace TravelAPI
