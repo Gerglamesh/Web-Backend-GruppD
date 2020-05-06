1804c6855ad2b68e9795894028d1c972d3b27e80
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-06 15:56:54 +0200

    @Gerglamesh add interface and instance class for CountryRepo

---
+++ TravelAPI/Services/CountryRepo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAPI.Models;
+
+namespace TravelAPI.Services
+{
+    public class CountryRepo
+    {
+        private readonly TravelAPIContext _travelAPIContext;
+        public CountryRepo(TravelAPIContext Context)
+        {
+            _travelAPIContext = Context;
+        }
+
+        public async Task<CountryModel> GetCountryModel()
+        {
+            var query = _travelAPIContext.CountryModel;
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
