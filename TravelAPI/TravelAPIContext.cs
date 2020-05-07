f9c62160d8a81e01e6a4d90da9530750c72640d7
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-07 10:37:46 +0200

    @Gerglamesh, Add test data to TravelAPIContext.cs

--- TravelAPI/TravelAPIContext.cs
+++ TravelAPI/TravelAPIContext.cs
@@ -27,5 +27,77 @@ namespace TravelAPI
             optionsBuilder.UseSqlServer(_travelAPIContext.GetConnectionString("TravelAPIContext"));
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CountryModel>()
+                .HasData(new
+                {
+                    CountryId = 1,
+                    Name = "Afghanistan",
+                    CountryInfo = 1,
+                });
+
+            modelBuilder.Entity<CountryInfoModel>()
+                .HasData(new
+                {
+                    CountryInfoModelID = 1,
+                    Population = 50000000,
+                ​	Governance = "Islamic Republic",
+                ​	CapitalCity = "Kabul",
+                ​	BNP = 19360000000,
+                ​	Area = 652000237,
+                ​	TimeZone = "GMT+4:30",
+                ​	NationalDay = 08 / 19,
+                ​	Language = "Dari/Pashtu/Iranian",
+                ​	RightHandTraffic = true,
+                    Country = 1
+                });
+
+            modelBuilder.Entity<CityModel>()
+                .HasData(new
+                {
+                    Name = "Kabul",
+                    Population = 4222000000,
+                    Country = 1
+                });
+
+            modelBuilder.Entity<CityModel>()
+                .HasData(new
+                {
+                    Name = "Balkh",
+                    Population = 1382200,
+                    Country = 1
+                });
+
+            modelBuilder.Entity<CityModel>()
+                .HasData(new
+                {
+                    Name = "Kandahar",
+                    Population = 614118,
+                    Country = 1
+                });
+
+            modelBuilder.Entity<AttractionModel>()
+                .HasData(new
+                {
+                    Name = "Buddha Niches",
+                    Location = "Bamiyan Valley, west of Kabul",
+                    Information = "The empty niches of the Buddha statues dominate the Bamiyan valley. Carved in the 6th century, the two statues, standing 38m and 55m respectively, were the tallest standing statues of Buddha ever made.",
+                    IsChildFriendly = true,
+                    Rating = 4,
+                    City = 1
+                });
+
+            modelBuilder.Entity<AttractionModel>()
+                .HasData(new
+                {
+                    Name = "Kabul Museum",
+                    Location = "Kabul",
+                    Information = "The Kabul Museum was once one of the greatest museums in the world. Its exhibits, ranging from Hellenistic gold coins to Buddhist statuary and Islamic bronzes, testified to Afghanistan’s location at the crossroads of Asia. After years of abuse during the civil war, help from the international community and the peerless dedication of its staff means the museum is slowly rising from the ashes. The museum opened in 1919, and was almost entirely stocked with items excavated in Afghanistan.",
+                    IsChildFriendly = true,
+                    Rating = 3,
+                    City = 1
+                });
+        }
     }
 }
