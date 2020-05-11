bda844b835cfec72b07920d1480468a5d43ca85f
fjalling <jakob.fjalling@gmail.com>
2020-05-11 14:06:13 +0200

    @fjalling fixed  according to requested changes

--- TravelAPI/Models/TravelRestrictionModel.cs
+++ TravelAPI/Models/TravelRestrictionModel.cs
@@ -8,20 +8,17 @@ namespace TravelAPI.Models
 {
     public class TravelRestrictionModel
     {
-        public class CountryModel
-        {
-            [Key]
-            public int CountryId { get; private set; }
-            public bool WorkTravelAllowed { get; private set; }
-            public bool TurismAllowed { get; private set; }
-            public bool ImmigrationAllowed { get; private set; }
-            public bool CitizenshipAllowed { get; private set; }
-            public bool FamilyVisitAllowed { get; private set; }
-            public bool IsVisaNeeded { get; private set; }
-            public int RiskLevel { get; private set; }
+        
+        [Key]
+        public int TravelRestrictionID { get; private set; }
+        public bool WorkTravelAllowed { get; private set; }
+        public bool TourismAllowed { get; private set; }
+        public bool ImmigrationAllowed { get; private set; }
+        public bool CitizenshipAllowed { get; private set; }
+        public bool FamilyVisitAllowed { get; private set; }
+        public bool IsVisaNeeded { get; private set; }
+        public int RiskLevel { get; private set; }
 
-            public ICollection<CityModel> Cities { get; set; }
-            public CountryInfoModel CountryInfo { get; set; }
-        }
+        public CountryModel Country { get; set; }
     }
 }
