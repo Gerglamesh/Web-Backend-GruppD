b5e1ad5c30439fcae6dd1503095ac31abd5d0643
fjalling <jakob.fjalling@gmail.com>
2020-05-11 13:26:11 +0200

    @fjalling Added country model added prop WorkTravel TRAPI-94

---
+++ TravelAPI/Models/TravelRestrictionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.Models
+{
+    public class TravelRestrictionModel
+    {
+        public class CountryModel
+        {
+            [Key]
+            public int CountryId { get; private set; }
+            public bool WorkTravelAllowed { get; private set; }
+
+
+            public ICollection<CityModel> Cities { get; set; }
+            public CountryInfoModel CountryInfo { get; set; }
+        }
+    }
+}
