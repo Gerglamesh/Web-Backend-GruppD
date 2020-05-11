f69650ddad575a112e97277447a33464269186e8
fjalling <jakob.fjalling@gmail.com>
2020-05-11 14:24:00 +0200

    @fjalling just saveing not finished

---
+++ TravelAPI/Services/TravelRestrictionRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.Services
+{
+    public class TravelRestrictionRepo
+    {
+    }
+}
