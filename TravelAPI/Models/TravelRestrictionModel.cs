775ec51daf7c2d64a65e9c4e12b02507937bab42
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-13 09:51:57 +0200

    @Gerglamesh, started work on unit test, had to make TravelRestrictionModel props have public sets instead of private to access them from test project.

--- TravelAPI/Models/TravelRestrictionModel.cs
+++ TravelAPI/Models/TravelRestrictionModel.cs
@@ -10,13 +10,13 @@ namespace TravelAPI.Models
     {
         
         [Key]
-        public int TravelRestrictionId { get; private set; }
-        public bool IsWorkTravelAllowed { get; private set; }
-        public bool IsTourismAllowed { get; private set; }
-        public bool IsImmigrationAllowed { get; private set; }
-        public bool IsCitizenshipAllowed { get; private set; }
-        public bool IsFamilyVisitAllowed { get; private set; }
-        public bool IsVisaNeeded { get; private set; }
-        public int RiskLevel { get; private set; }
+        public int TravelRestrictionId { get; set; }
+        public bool IsWorkTravelAllowed { get; set; }
+        public bool IsTourismAllowed { get; set; }
+        public bool IsImmigrationAllowed { get; set; }
+        public bool IsCitizenshipAllowed { get; set; }
+        public bool IsFamilyVisitAllowed { get; set; }
+        public bool IsVisaNeeded { get; set; }
+        public int RiskLevel { get; set; }
     }
 }
---
+++ TravelAPITests/Services/CountryRepoTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelAPI.Models;
+
+namespace TravelAPI.Services.Tests
+{
+    [TestClass()]
+    public class CountryRepoTests
+    {
+        [TestMethod()]
+        public void GetCountriesTest()
+        {
+            //Arrange
+            IList<CountryModel> countries = GenerateEvents();
+            //Act 
+
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCountryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCountryTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetRightHandTrafficTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCountriesByLanguageTest()
+        {
+            Assert.Fail();
+        }
+
+        private static IList<CountryModel> GenerateEvents()
+        {
+            return new List<CountryModel>
+            {
+                new CountryModel
+                {
+                    CountryId = 1,
+                    Name = "Wakanda",
+                    CountryInfo = new CountryInfoModel
+                    {
+                        CountryInfoId = 1,
+                        Population = 50000,
+                        Governance = "Monarchy",
+                        BNP = 500000000,
+                        CapitalCity = "Birnin Zana",
+                        Area = 50000,
+                        TimeZone = "GMT + 3",
+                        NationalDay = "31/3",
+                        RightHandTraffic = true,
+                        Language = "Wakandan/Yoruba/Hausa/English"
+                    },
+                    TravelRestriction = new TravelRestrictionModel
+                    {
+                        TravelRestrictionId = 1,
+
+                    }
+                }
+            };
+        }
+    }
+}
