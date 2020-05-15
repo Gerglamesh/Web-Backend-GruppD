cd5222ebeb74e21f0d5fe11f01f7ef09ae5fd2c2
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-15 12:42:27 +0200

    Add @fjalling code refinements and additions to repo, interface and model of City

--- TravelAPI/Services/CityRepo.cs
+++ TravelAPI/Services/CityRepo.cs
@@ -10,20 +10,65 @@ namespace TravelAPI.Services
     public class CityRepo : Repository, ICityRepo
     {
         private readonly TravelAPIContext _travelApiContext;
-        public CityRepo(TravelAPIContext travelAPIContext, ILogger<CountryRepo> logger) : base(travelAPIContext, logger)
+        public CityRepo(TravelAPIContext travelAPIContext, ILogger<CityRepo> logger) : base(travelAPIContext, logger)
         {
             _travelApiContext = travelAPIContext;
         }
 
-        public async Task<CityModel> GetCity(string Name)
+        public async Task<ICollection<CityModel>> GetCities(
+            bool IncludeAttractions = false,
+            bool IncludeCountries = false)
         {
-            var query = _travelApiContext.Cities
-            .Where(s => s.Name == Name);
+            _logger.LogInformation("Getting Cities");
+            IQueryable<CityModel> query = _travelAPIContext.Cities
+                .Include(a => a.Attractions);
+            if (IncludeAttractions)
+            {
+                query.Include(a => a.Attractions);
+            }
+            if (IncludeCountries)
+            {
+                query.Include(c => c.Country);
+            }
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<CityModel> GetCityByName(
+            string name,
+            bool IncludeCoutries = false,
+            bool IncludeAttractions = false)
+        {
+            _logger.LogInformation($"Getting City named '{name}')");
+
+            IQueryable<CityModel> query = _travelApiContext.Cities.Where(n => n.Name == name);
+            if (IncludeCoutries)
+            {
+                query = query.Include(c => c.Country);
+            }
+            if (IncludeAttractions)
+            {
+                query = query.Include(a => a.Attractions);
+            }
             return await query.FirstOrDefaultAsync();
         }
-        public async Task<ICollection<CityModel>> GetCities(bool includeAttractions = false)
+
+        public async Task<CityModel> GetCityById(
+            int CityId,
+            bool IncludeCoutries = false,
+            bool IncludeAttractions = false)
         {
-            return await _travelApiContext.Set<CityModel>().ToListAsync();
+            _logger.LogInformation("Getting City by Id");
+
+            IQueryable<CityModel> query = _travelApiContext.Cities.Where(i => i.CityId == CityId);
+            if (IncludeCoutries)
+            {
+                query = query.Include(c => c.Country);
+            }
+            if (IncludeAttractions)
+            {
+                query = query.Include(a => a.Attractions);
+            }
+            return await query.FirstOrDefaultAsync();
         }
     }
 }
