f8e81f994ba51ec93b64ae09f6a67bfb8861f679
irvin <irvin_b23@hotmail.com>
2020-05-15 14:49:54 +0200

    @irvper77 add dtosetup to projekt

---
+++ TravelAPI/Configuration/MappedProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using TravelAPI.DTO;
+using TravelAPI.Models;
+
+namespace TravelAPI.Services
+{
+    public class MappedProfile : Profile
+    {
+        public MappedProfile()
+        {
+            /*
+            CreateMap<CountryModel, CountryDto>()
+                .ReverseMap();
+
+            CreateMap<, GigDto>()
+                .ReverseMap();
+
+            CreateMap<Venue, VenueDto>()
+                .ReverseMap();
+
+            CreateMap<Artist, ArtistDto>()
+                .ReverseMap();
+                */
+        }
+
+    }
+}
