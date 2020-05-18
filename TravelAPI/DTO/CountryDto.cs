e8e21080e3af78046f3e7f952e9e547c8941232a
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-18 15:37:02 +0200

    @Gerglamesh code cleanup in CountryDto.cs

--- TravelAPI/DTO/CountryDto.cs
+++ TravelAPI/DTO/CountryDto.cs
@@ -6,14 +6,12 @@ namespace TravelAPI.DTO
 {
     public class CountryDto
     {
-        [Required]
-        public int CountryId { get; set; }
-        [Required]
-        public string Name { get; set; }
+        [Required] public int CountryId { get; set; }
+        [Required] public string Name { get; set; }
 
         //Relationships
-        public CountryInfoModel CountryInfo { get; set; }
-        public ICollection<CityModel> Cities { get; set; }
+        public CountryInfoDto CountryInfo { get; set; }
+        public ICollection<CityDto> Cities { get; set; }
         public TravelRestrictionModel TravelRestriction { get; set; }
     }
 }
