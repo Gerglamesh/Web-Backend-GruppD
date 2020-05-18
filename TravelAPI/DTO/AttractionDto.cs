a16a3201659bbb314c39802675ea36755ffe0d68
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-18 15:27:45 +0200

    @Gerglamesh code cleanup in AttractionDto.cs

--- TravelAPI/DTO/AttractionDto.cs
+++ TravelAPI/DTO/AttractionDto.cs
@@ -9,18 +9,11 @@ namespace TravelAPI.DTO
 {
     public class AttractionDto
     {
-
-        [Required]
-        public int AttractionId { get; set; }
-        [Required]
-        public string Name { get; set; }
-        
-        public string Location { get; set; }
-        
-        public bool IsChildFriendly { get; set; }
-        
-        public string Information { get; set; }
-       
+        [Required] public int AttractionId { get; set; }
+        [Required] public string Name { get; set; }        
+        public string Location { get; set; }        
+        public bool IsChildFriendly { get; set; }        
+        public string Information { get; set; }       
         public int Rating { get; set; }
 
         //Relationships
