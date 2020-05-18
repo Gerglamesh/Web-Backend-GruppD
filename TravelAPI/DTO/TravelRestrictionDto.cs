93cc2cc8f4d0ee0f1198ef04060421d75481d2bb
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-18 15:41:51 +0200

    @Gerglamesh code cleanup in TravelRestrictionDto.cs

--- TravelAPI/DTO/TravelRestrictionDto.cs
+++ TravelAPI/DTO/TravelRestrictionDto.cs
@@ -4,21 +4,13 @@ namespace TravelAPI.DTO
 {
     public class TravelRestrictionDto
     {
-        [Required]
-        public int TravelRestrictionId { get; set; }
-        [Required]
-        public bool IsWorkTravelAllowed { get; set; }
-        [Required]
-        public bool IsTourismAllowed { get; set; }
-        [Required]
-        public bool IsImmigrationAllowed { get; set; }
-        [Required]
-        public bool IsCitizenshipAllowed { get; set; }
-        [Required]
-        public bool IsFamilyVisitAllowed { get; set; }
-        [Required]
-        public bool IsVisaNeeded { get; set; }
-        [Required]
-        public int RiskLevel { get; set; }
+        [Required] public int TravelRestrictionId { get; set; }
+        [Required] public bool IsWorkTravelAllowed { get; set; }
+        [Required] public bool IsTourismAllowed { get; set; }
+        [Required] public bool IsImmigrationAllowed { get; set; }
+        [Required] public bool IsCitizenshipAllowed { get; set; }
+        [Required] public bool IsFamilyVisitAllowed { get; set; }
+        [Required] public bool IsVisaNeeded { get; set; }
+        [Required] public int RiskLevel { get; set; }
     }
 }
