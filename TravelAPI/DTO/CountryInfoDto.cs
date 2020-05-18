98f05bb51946791cb82e8e061fdf144d39a72648
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-05-18 15:41:04 +0200

    @Gerglamesh code cleanup in CountryInfoDto.cs

--- TravelAPI/DTO/CountryInfoDto.cs
+++ TravelAPI/DTO/CountryInfoDto.cs
@@ -8,25 +8,15 @@ namespace TravelAPI.DTO
 {
     public class CountryInfoDto
     {
-        [Required]
-        public int CountryInfoId { get; set; }
-        [Required]
-        public int Population { get; set; } 
-        [Required]
-        public string Governance { get; set; }
-        [Required]
-        public int BNP { get; set; }
-        [Required]
-        public string CapitalCity { get; set; }
-        [Required]
-        public int Area { get; set; }
-        [Required]
-        public string TimeZone { get; set; }
-        [Required]
-        public string NationalDay { get; set; }
-        [Required]
-        public bool RightHandTraffic { get; set; }
-        [Required]
-        public string Language { get; set; }
+        [Required] public int CountryInfoId { get; set; }
+        [Required] public int Population { get; set; } 
+        [Required] public string Governance { get; set; }
+        [Required] public int BNP { get; set; }
+        [Required] public string CapitalCity { get; set; }
+        [Required] public int Area { get; set; }
+        [Required] public string TimeZone { get; set; }
+        [Required] public string NationalDay { get; set; }
+        [Required] public bool RightHandTraffic { get; set; }
+        [Required] public string Language { get; set; }
     }
 }
