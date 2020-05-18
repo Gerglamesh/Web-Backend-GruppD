7d64c4b077d75d6d8cb67b138b260da0a7eda9b9
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-05-18 11:58:55 +0200

    @MirkoPralica Add Class CountryInfoDTO

---
+++ TravelAPI/DTO/CountryInfoDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAPI.DTO
+{
+    public class CountryInfoDto
+    {
+        [Required]
+        public int CountryInfoId { get; set; }
+        [Required]
+        public int Population { get; set; } 
+        [Required]
+        public string Governance { get; set; }
+        [Required]
+        public int BNP { get; set; }
+        [Required]
+        public string CapitalCity { get; set; }
+        [Required]
+        public int Area { get; set; }
+        [Required]
+        public string TimeZone { get; set; }
+        [Required]
+        public string NationalDay { get; set; }
+        [Required]
+        public bool RightHandTraffic { get; set; }
+        [Required]
+        public string Language { get; set; }
+    }
+}
---
+++ TravelAPI/Migrations/20200518093215_18-maj.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAPI.Migrations
+{
+    public partial class _18maj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
