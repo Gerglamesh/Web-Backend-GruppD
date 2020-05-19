51c5ea42114c7e05a484e035e1a6c86e86949df4
oskar morell <oskarmorell1998@hotmail.se>
2020-05-19 11:55:40 +0200

    @oskarmorell Added RegularExpression to string properties and Range to int properties

--- TravelAPI/DTO/CityDto.cs
+++ TravelAPI/DTO/CityDto.cs
@@ -5,8 +5,13 @@ namespace TravelAPI.DTO
 {
     public class CityDto
     {
-        [Required] public int CityId { get; set; }
-        [Required] public string Name { get; set; }
+        [Required] 
+        public int CityId { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,85}$",
+            ErrorMessage = "Characters are not allowed or text is to long/short.")]
+        public string Name { get; set; }
+        [Range(1, 2147483647, ErrorMessage = "Value for {0} must be between {1} and {2}")] 
         public int Population { get; set; }
 
         public CountryDto Country { get; set; }
