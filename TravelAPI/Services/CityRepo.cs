0b12f6199586c4e6ed9f10e4af2aac55da9055f2
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-25 14:30:57 +0200

    Refactored CityRepo an IRepo
    Also added a method that handles all inludes

--- TravelAPI/Services/CityRepo.cs
+++ TravelAPI/Services/CityRepo.cs
@@ -14,52 +14,99 @@ namespace TravelAPI.Services
         {
         }
 
-        private IQueryable<CityModel> Include(bool includeAttractions, bool includeCountries, IQueryable<CityModel> query)
+        private IQueryable<CityModel> Include(
+            IQueryable<CityModel> query,
+            bool includeCoutry = false,
+            int minPopulation = 0,
+            int maxPopulation = 0,
+            int minRating = 0,
+            int maxRating = 0)
         {
-            if (includeAttractions)
-            {
-                query.Include(a => a.Attractions);
-            }
-            if (includeCountries)
+            if (includeCoutry)
             {
                 query.Include(c => c.Country);
             }
+
+            if (minPopulation > 0 && maxPopulation > 0)
+            {
+                query.Where(p => p.Population > minPopulation && p.Population < maxPopulation);
+            }
+            else if (minPopulation > 0)
+            {
+                query.Where(p => p.Population > minPopulation);
+            }
+            else if(maxPopulation > 0)
+            {
+                query.Where(p => p.Population < maxPopulation);
+            }
+
+            if (minRating > 0 && maxRating > 0)
+            {
+            }
+
             return query;
         }
 
         public async Task<ICollection<CityModel>> GetCities(
-            bool includeAttractions = false,
-            bool includeCountries = false)
+            bool includeCoutry = false,
+            int minPopulation = 0,
+            int maxPopulation = 0,
+            int minRating = 0,
+            int maxRating = 0)
         {
             _logger.LogInformation("Getting Cities.");
             IQueryable<CityModel> query = _travelAPIContext.Cities;
   
-            return await Include(includeAttractions, includeCountries, query)
+            return await Include(
+                query, 
+                includeCoutry, 
+                minPopulation, 
+                maxPopulation, 
+                minRating, 
+                maxRating)
                 .ToArrayAsync();
         }
 
 
         public async Task<CityModel> GetCityByName(
             string name,
-            bool includeCountries = false,
-            bool includeAttractions = false)
+            bool includeCoutry = false,
+            int minPopulation = 0,
+            int maxPopulation = 0,
+            int minRating = 0,
+            int maxRating = 0)
         {
             _logger.LogInformation($"Getting City named '{name}')");
             IQueryable<CityModel> query = _travelApiContext.Cities.Where(n => n.Name == name);
 
-            return await Include(includeAttractions, includeCountries, query)
+            return await Include(
+                query,
+                includeCoutry,
+                minPopulation,
+                maxPopulation,
+                minRating,
+                maxRating)
                 .SingleOrDefaultAsync();
         }
 
         public async Task<CityModel> GetCityById(
             int cityId,
-            bool includeCountries = false,
-            bool includeAttractions = false)
+            bool includeCoutry = false,
+            int minPopulation = 0,
+            int maxPopulation = 0,
+            int minRating = 0,
+            int maxRating = 0)
         {
             _logger.LogInformation($"Getting City by ID: {cityId}");
             IQueryable<CityModel> query = _travelApiContext.Cities.Where(i => i.CityId == cityId);
 
-            return await Include(includeAttractions, includeCountries, query)
+            return await Include(
+                query,
+                includeCoutry,
+                minPopulation,
+                maxPopulation,
+                minRating,
+                maxRating)
                 .SingleOrDefaultAsync();
         }
     }
