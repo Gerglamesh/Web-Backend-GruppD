3fff5bab2af45aa93c617eb1a7077eae607719a2
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-06-01 20:10:35 +0200

    auth0 is working

--- TravelAPI/Startup.cs
+++ TravelAPI/Startup.cs
@@ -16,14 +16,17 @@ namespace TravelAPI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //var config = new ConfigurationBuilder()
-            //    .AddJsonFile("appsettings.json")
-            //    .Build();
 
             services.AddAuthentication(options =>
             {
@@ -31,8 +34,8 @@ namespace TravelAPI
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(options =>
             {
-                //options.Authority = $"https://{configuration["Auth0:Domain"]}/";
-                //options.Audience =  configuration["Auth0:Audience"];
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                options.Audience = Configuration["Auth0:Audience"];
             });
             services.AddControllers()
                 .AddNewtonsoftJson(options =>
